1837bb172276d691823abab6f92dbc90b88c6304
kenn-hawley <50890153+kenn-hawley@users.noreply.github.com>
2026-07-28 23:46:51 +0100

    Update __init__.py

--- esphome/components/ble_button_advertiser/__init__.py
+++ esphome/components/ble_button_advertiser/__init__.py
@@ -1,5 +1,6 @@
 import esphome.config_validation as cv
 import esphome.codegen as cg
+import esphome.automation as automation
 from esphome import pins
 from esphome.const import CONF_ID
 
@@ -7,6 +8,7 @@ CONF_PIN = "pin"
 
 ns = cg.esphome_ns.namespace("ble_button_advertiser")
 BleButtonAdvertiser = ns.class_("BleButtonAdvertiser", cg.Component)
+AdvertisePressAction = ns.class_("AdvertisePressAction", cg.Action)
 
 CONFIG_SCHEMA = cv.COMPONENT_SCHEMA.extend({
     cv.GenerateID(): cv.declare_id(BleButtonAdvertiser),
@@ -20,3 +22,16 @@ async def to_code(config):
     if CONF_PIN in config:
         pin = await cg.gpio_pin_expression(config[CONF_PIN])
         cg.add(var.set_pin(pin))
+
+
+@automation.register_action(
+    "ble_button_advertiser.advertise_press",
+    AdvertisePressAction,
+    cv.Schema({
+        cv.Required(cv.GenerateID()): cv.use_id(BleButtonAdvertiser),
+    })
+)
+async def advertise_press_to_code(config, action_id, template_arg, args):
+    paren = await cg.get_variable(config[cv.GenerateID()])
+    var = cg.new_Pvariable(action_id, template_arg, paren)
+    return var
